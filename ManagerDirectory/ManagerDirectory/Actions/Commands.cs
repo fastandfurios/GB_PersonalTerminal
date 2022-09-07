a9a1b21b9659d9329b018e24b4ff29f4ab383009
Дмитрий <semu-dima@yandex.ru>
2022-09-07 21:47:55 +0300

    cleanup code

--- ManagerDirectory/ManagerDirectory/Actions/Commands.cs
+++ ManagerDirectory/ManagerDirectory/Actions/Commands.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ManagerDirectory.Actions
+﻿namespace ManagerDirectory.Actions
 {
 	readonly struct Commands
 	{
