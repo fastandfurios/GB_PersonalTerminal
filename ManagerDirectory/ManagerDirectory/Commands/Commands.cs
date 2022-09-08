5d39eb927a769c054b3565501e9aff9866d8fd1e
Дмитрий <semu-dima@yandex.ru>
2022-09-08 14:11:00 +0300

    added commands

---
+++ ManagerDirectory/ManagerDirectory/Commands/Commands.cs
@@ -0,0 +1,21 @@
+﻿namespace ManagerDirectory.Commands
+{
+	readonly struct Commands
+	{
+		public string[] ArrayCommands => new string[]
+		{
+			"disk",
+			"ls",
+			"lsAll",
+			"cp",
+			"rm",
+			"info",
+			"clear",
+			"cd",
+			"cd..",
+			"cd\\",
+			"help",
+			"exit"
+		};
+    }
+}
